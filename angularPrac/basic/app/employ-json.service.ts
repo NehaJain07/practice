27a0c901340d94ea537b16a72c477c5435cbc480
neha <jainn.ne@gmail.com>
2022-04-13 20:09:41 +0530

    fix json error

--- angularPrac/basic/app/employ-json.service.ts
+++ angularPrac/basic/app/employ-json.service.ts
@@ -1,19 +1,20 @@
-import { HttpClient, HttpErrorResponse,  HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { catchError, throwError } from 'rxjs';
 import { IEmployee } from './employee';
-import { throwError, catchError } from 'rxjs';
-
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class EmployJsonService {
-    private _url: string = "assets/data/employee.json"
-    constructor(private _http: HttpClient) { }
-    getEmployee(): Observable<IEmployee[]> {
-        return this._http.get<IEmployee[]>(this._url).pipe(catchError(this.ErrorHandle))
-    }
-    ErrorHandle(error: HttpErrorResponse) {
-        return throwError(error.message)
-    }
-} 
+    private _url:string="/assets/data/employee.json" 
+  
+    constructor(private _http:HttpClient) { } 
+    getEmployee(): Observable<IEmployee[]>{ 
+      return this._http.get<IEmployee[]>(this._url).pipe(catchError(this.ErrorHandle)) 
+    } 
+    ErrorHandle(error: HttpErrorResponse){ 
+      return throwError(error.message) 
+    } 
+  
+}
