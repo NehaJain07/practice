737b48d9f45c1a2bfc2131db3a5c251e70f0fefd
neha <jainn.ne@gmail.com>
2022-04-24 19:05:43 +0530

    remove extra files

--- angularPrac/angRel/25 - Bonus - Angular Universal/recipebook-manual/src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  firebaseAPIKey: 'AIzaSyAyIoiUU3mv6DKBiPpUKwTlaZmtJ_5lmlo'
-};
--- angularPrac/angRel/28 - A Basic Introduction to Unit Testing in/testingapp/src/app/shared/reverse.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ReversePipe } from './reverse.pipe';
-
-describe('UserComponent', () => {
-  it('should create the app', () => {
-    let reversePipe = new ReversePipe();
-    expect(reversePipe.transform('hello')).toEqual('olleh');
-  });
-});
